abfe9344923aa2660a5ff62d6aa4ce4bf69465d6
Sanjay <sanjhrock@gmail.com>
2019-09-20 12:53:39 +0530

    Handling Orientation

--- app/src/main/java/com/panic1k/timefighter/MainActivity.kt
+++ app/src/main/java/com/panic1k/timefighter/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     internal var score = 0
     internal var gameStarted = false
     internal lateinit var countDownTimer: CountDownTimer
-    internal val initialCountdown: Long = 60000
+    internal val initialCountDown: Long = 60000
     internal val countDownInterval: Long = 1000
     internal val TAG = MainActivity::class.java.simpleName
     internal var timeLeftOnTimer: Long = 60000
@@ -27,69 +27,55 @@ class MainActivity : AppCompatActivity() {
         private val TIME_LEFT_KEY = "TIME_LEFT_KEY"
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "On Create Called. Score is: $score")
+        Log.d(TAG, "onCreate called. Score is: $score")
 
         tapMeButton = findViewById<Button>(R.id.tap_me_button)
         gameScoreTextView = findViewById<TextView>(R.id.game_score_text_view)
-        timeLeftTextView = findViewById(R.id.time_left_text_view)
-        gameScoreTextView.text = getString(R.string.your_score, score.toString())
-        resetgame()
+        timeLeftTextView = findViewById<TextView>(R.id.time_left_text_view)
+        resetGame()
 
         tapMeButton.setOnClickListener {
             incrementScore()
-
         }
-
     }
 
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
         outState.putInt(SCORE_KEY, score)
         outState.putLong(TIME_LEFT_KEY, timeLeftOnTimer)
         countDownTimer.cancel()
-        Log.d(TAG, "onSaveInstanceState: Saving Score : $score & Time Left: $timeLeftOnTimer")
+        Log.d(TAG, "onSaveInstanceState: Saving Score: $score & Time Left: $timeLeftOnTimer")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG, "onDestroy Called.")
+
+        Log.d(TAG, "onDestroy called.")
     }
-    private fun resetgame() {
+
+
+    private fun resetGame() {
         score = 0
         gameScoreTextView.text = getString(R.string.your_score, score.toString())
-        val initialTiemLeft = initialCountdown / 1000
-        timeLeftTextView.text = getString(R.string.time_left, initialTiemLeft.toString())
+        val initialTimeLeft = initialCountDown / 1000
+        timeLeftTextView.text = getString(R.string.time_left, initialTimeLeft.toString())
 
-        countDownTimer = object : CountDownTimer(initialCountdown, countDownInterval) {
-
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
             override fun onTick(millisUntilFinished: Long) {
                 timeLeftOnTimer = millisUntilFinished
                 val timeLeft = millisUntilFinished / 1000
                 timeLeftTextView.text = getString(R.string.time_left, timeLeft.toString())
-
-
             }
 
             override fun onFinish() {
-
+                endGame()
             }
-
         }
         gameStarted = false
-
-
-    }
-
-    private fun endGame() {
-        Toast.makeText(
-            this,
-            getString(R.string.game_over_message, score.toString()),
-            Toast.LENGTH_SHORT
-        )
-        resetgame()
     }
 
     private fun startGame() {
@@ -97,15 +83,22 @@ class MainActivity : AppCompatActivity() {
         gameStarted = true
     }
 
-    private fun incrementScore() {
+    private fun endGame() {
+        Toast.makeText(
+            this,
+            getString(R.string.game_over_message, score.toString()),
+            Toast.LENGTH_SHORT
+        ).show()
+        resetGame()
+    }
 
+    private fun incrementScore() {
         if (!gameStarted) {
             startGame()
         }
         score = score + 1
         val newScore = getString(R.string.your_score, score.toString())
         gameScoreTextView.text = newScore
-
 
     }
 
